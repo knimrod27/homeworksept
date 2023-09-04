3eda92bbf23b4b67de8052e6be480bced72c1349
knimrod27 <nemfelejtemel14@gmail.com>
2023-09-04 10:38:49 +0300

    homework

--- src/Product.java
+++ src/Product.java
@@ -4,7 +4,6 @@ public class Product {
     private int quantity;
     private String category;
 
-    // Constructor to initialize the Product object
     public Product(String name, double price, int quantity, String category) {
         this.name = name;
         this.price = price;
@@ -12,37 +11,34 @@ public class Product {
         this.category = category;
     }
 
-    // Getter method for name
+
     public String getName() {
         return name;
     }
 
-    // Getter method for price
+
     public double getPrice() {
         return price;
     }
 
-    // Getter method for quantity
     public int getQuantity() {
         return quantity;
     }
 
-    // Getter method for category
+
     public String getCategory() {
         return category;
     }
 
-    // Check if the product has stock (quantity is not 0)
     public boolean hasStock() {
         return quantity > 0;
     }
 
-    // Check if the product belongs to a specific category
+
     public boolean isCategory(String categoryToCheck) {
         return category.equals(categoryToCheck);
     }
 
-    // Override the toString() method to provide a human-readable representation of the object
     @Override
     public String toString() {
         return "Product{" +
